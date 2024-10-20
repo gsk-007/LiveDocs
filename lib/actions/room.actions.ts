77a05d4b0549bcb5286badff5f5456b5850d3067
gsk-007 <gsingh16742@gmail.com>
2024-10-20 15:02:55 +0530

    (feat): edit document title

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -35,3 +35,39 @@ export const createDocument = async ({
     console.log(`Error happened while creating a room: ${err}`);
   }
 };
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    if (!hasAccess) {
+      throw new Error("You do not have access to this document");
+    }
+    return parseStringify(room);
+  } catch (err) {
+    console.log(`Error happened while getting a room: ${err}`);
+  }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+    revalidatePath(`/documents/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (err) {
+    console.log(`Error happened while updating a room: ${err}`);
+  }
+};
